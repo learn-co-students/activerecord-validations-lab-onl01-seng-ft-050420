8c84ffbf78c7930a7962829aa819cfe4bceee497
scottvonscott <skeplin17@gmail.com>
2020-07-13 14:46:18 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait_y
+
+    
+
+    
+    BUZZWORDS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top[0-9]*/i,
+        /Guess/i]
+
+    def clickbait_y
+        if BUZZWORDS.none? {|buzz| buzz.match title}
+            errors.add(:title, "That isn't clickbait-y enough!")
+        end
+    end
 end
+  
+
